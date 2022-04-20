1baf38ccdf389ae3d702e3b2729b20e91d0618ca
Santanu Sinha <santanu.sinha@gmail.com>
2022-04-20 17:22:32 +0530

    Added integration test for app engine

---
+++ drove-controller/src/main/java/com/phonepe/drove/controller/engine/RandomInstanceIdGenerator.java
@@ -0,0 +1,11 @@
+package com.phonepe.drove.controller.engine;
+
+/**
+ *
+ */
+public class RandomInstanceIdGenerator implements InstanceIdGenerator {
+    @Override
+    public String generateInstanceId() {
+        return null;
+    }
+}
