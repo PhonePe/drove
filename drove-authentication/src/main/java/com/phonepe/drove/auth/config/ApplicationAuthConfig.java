c3b7dbdeeb1d59c3b44d081547e1a323851fbe4d
Santanu Sinha <santanu.sinha@gmail.com>
2022-07-26 13:28:00 +0530

    Added required annotation on instance auth

--- drove-authentication/src/main/java/com/phonepe/drove/auth/config/ApplicationAuthConfig.java
+++ drove-authentication/src/main/java/com/phonepe/drove/auth/config/ApplicationAuthConfig.java
@@ -1,11 +1,15 @@
 package com.phonepe.drove.auth.config;
 
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 /**
  *
  */
 @Value
+@Jacksonized
+@Builder
 public class ApplicationAuthConfig {
     public static final ApplicationAuthConfig DEFAULT = new ApplicationAuthConfig("DEFAULT_SECRET");
 
