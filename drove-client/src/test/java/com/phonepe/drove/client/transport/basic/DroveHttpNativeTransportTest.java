6efaa7e3f6db48585bf3511892760470d2a7329c
Santanu Sinha <santanu.sinha@gmail.com>
2023-01-16 12:55:33 +0530

    Additional tests

---
+++ drove-client/src/test/java/com/phonepe/drove/client/transport/basic/DroveHttpNativeTransportTest.java
@@ -0,0 +1,206 @@
+package com.phonepe.drove.client.transport.basic;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.phonepe.drove.client.DroveClient;
+import com.phonepe.drove.client.DroveClientConfig;
+import com.phonepe.drove.client.DroveHttpTransport;
+import lombok.SneakyThrows;
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
+@WireMockTest
+class DroveHttpNativeTransportTest {
+    @Test
+    @SneakyThrows
+    void testGet(WireMockRuntimeInfo wm) {
+        stubFor(get(DroveClient.PING_API)
+                        .withHeader("TestHeader", equalTo("TestValue"))
+                        .willReturn(ok()));
+        try(val t = create(clientConfig(wm))) {
+            Assertions.assertTrue(
+                    t.execute(new DroveHttpTransport.TransportRequest(DroveClient.Method.GET,
+                                                                      URI.create(wm.getHttpBaseUrl() + DroveClient.PING_API),
+                                                                      Map.of("TestHeader", List.of("TestValue")),
+                                                                      null),
+                              new DroveClient.ResponseHandler<Boolean>() {
+                                  @Override
+                                  public Boolean defaultValue() {
+                                      return false;
+                                  }
+
+                                  @Override
+                                  public Boolean handle(DroveClient.Response response) {
+                                      return response.statusCode() == 200;
+                                  }
+                              }));
+            Assertions.assertFalse(
+                    t.execute(new DroveHttpTransport.TransportRequest(DroveClient.Method.GET,
+                                                                      URI.create(wm.getHttpBaseUrl() + DroveClient.PING_API),
+                                                                      Map.of("TestHeader", List.of("TestValue")),
+                                                                      null),
+                              new DroveClient.ResponseHandler<Boolean>() {
+                                  @Override
+                                  public Boolean defaultValue() {
+                                      return false;
+                                  }
+
+                                  @Override
+                                  public Boolean handle(DroveClient.Response response) {
+                                      throw new IllegalStateException("Test failure");
+                                  }
+                              }));
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    void testPost(WireMockRuntimeInfo wm) {
+        stubFor(post(DroveClient.PING_API)
+                        .withHeader("TestHeader", equalTo("TestValue"))
+                        .withRequestBody(equalTo("TestBody"))
+                        .willReturn(ok()));
+        try(val t = create(clientConfig(wm))) {
+            Assertions.assertTrue(
+                    t.execute(new DroveHttpTransport.TransportRequest(DroveClient.Method.POST,
+                                                                      URI.create(wm.getHttpBaseUrl() + DroveClient.PING_API),
+                                                                      Map.of("TestHeader", List.of("TestValue")),
+                                                                      "TestBody"),
+                              new DroveClient.ResponseHandler<Boolean>() {
+                                  @Override
+                                  public Boolean defaultValue() {
+                                      return false;
+                                  }
+
+                                  @Override
+                                  public Boolean handle(DroveClient.Response response) {
+                                      return response.statusCode() == 200;
+                                  }
+                              }));
+            Assertions.assertFalse(
+                    t.execute(new DroveHttpTransport.TransportRequest(DroveClient.Method.POST,
+                                                                      URI.create(wm.getHttpBaseUrl() + DroveClient.PING_API),
+                                                                      Map.of("TestHeader", List.of("TestValue")),
+                                                                      "TestBody"),
+                              new DroveClient.ResponseHandler<Boolean>() {
+                                  @Override
+                                  public Boolean defaultValue() {
+                                      return false;
+                                  }
+
+                                  @Override
+                                  public Boolean handle(DroveClient.Response response) {
+                                      throw new IllegalStateException("Test failure");
+                                  }
+                              }));
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    void testPut(WireMockRuntimeInfo wm) {
+        stubFor(put(DroveClient.PING_API)
+                        .withHeader("TestHeader", equalTo("TestValue"))
+                        .withRequestBody(equalTo("TestBody"))
+                        .willReturn(ok()));
+        try(val t = create(clientConfig(wm))) {
+            Assertions.assertTrue(
+                    t.execute(new DroveHttpTransport.TransportRequest(DroveClient.Method.PUT,
+                                                                      URI.create(wm.getHttpBaseUrl() + DroveClient.PING_API),
+                                                                      Map.of("TestHeader", List.of("TestValue")),
+                                                                      "TestBody"),
+                              new DroveClient.ResponseHandler<Boolean>() {
+                                  @Override
+                                  public Boolean defaultValue() {
+                                      return false;
+                                  }
+
+                                  @Override
+                                  public Boolean handle(DroveClient.Response response) {
+                                      return response.statusCode() == 200;
+                                  }
+                              }));
+            Assertions.assertFalse(
+                    t.execute(new DroveHttpTransport.TransportRequest(DroveClient.Method.PUT,
+                                                                      URI.create(wm.getHttpBaseUrl() + DroveClient.PING_API),
+                                                                      Map.of("TestHeader", List.of("TestValue")),
+                                                                      "TestBody"),
+                              new DroveClient.ResponseHandler<Boolean>() {
+                                  @Override
+                                  public Boolean defaultValue() {
+                                      return false;
+                                  }
+
+                                  @Override
+                                  public Boolean handle(DroveClient.Response response) {
+                                      throw new IllegalStateException("Test failure");
+                                  }
+                              }));
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    void testDelete(WireMockRuntimeInfo wm) {
+        stubFor(delete(DroveClient.PING_API)
+                        .withHeader("TestHeader", equalTo("TestValue"))
+                        .willReturn(ok()));
+        try(val t = create(clientConfig(wm))) {
+            Assertions.assertTrue(
+                    t.execute(new DroveHttpTransport.TransportRequest(DroveClient.Method.DELETE,
+                                                                      URI.create(wm.getHttpBaseUrl() + DroveClient.PING_API),
+                                                                      Map.of("TestHeader", List.of("TestValue")),
+                                                                      null),
+                              new DroveClient.ResponseHandler<Boolean>() {
+                                  @Override
+                                  public Boolean defaultValue() {
+                                      return false;
+                                  }
+
+                                  @Override
+                                  public Boolean handle(DroveClient.Response response) {
+                                      return response.statusCode() == 200;
+                                  }
+                              }));
+            Assertions.assertFalse(
+                    t.execute(new DroveHttpTransport.TransportRequest(DroveClient.Method.DELETE,
+                                                                      URI.create(wm.getHttpBaseUrl() + DroveClient.PING_API),
+                                                                      Map.of("TestHeader", List.of("TestValue")),
+                                                                      null),
+                              new DroveClient.ResponseHandler<Boolean>() {
+                                  @Override
+                                  public Boolean defaultValue() {
+                                      return false;
+                                  }
+
+                                  @Override
+                                  public Boolean handle(DroveClient.Response response) {
+                                      throw new IllegalStateException("Test failure");
+                                  }
+                              }));
+        }
+    }
+
+    private static DroveClientConfig clientConfig(WireMockRuntimeInfo wm) {
+        return new DroveClientConfig(List.of(wm.getHttpBaseUrl()),
+                                     Duration.ofSeconds(1),
+                                     Duration.ofSeconds(1),
+                                     Duration.ofSeconds(1));
+    }
+
+    private DroveHttpTransport create(final DroveClientConfig clientConfig) {
+        return new DroveHttpNativeTransport(clientConfig);
+    }
+}
