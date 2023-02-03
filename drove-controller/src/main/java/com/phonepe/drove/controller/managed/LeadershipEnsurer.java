6acbf03a86a1f7789a502689b5786dec8b40608c
Santanu Sinha <santanu.sinha@gmail.com>
2023-02-03 15:55:15 +0530

    Remimplemented using LeaderSelector

--- drove-controller/src/main/java/com/phonepe/drove/controller/managed/LeadershipEnsurer.java
+++ drove-controller/src/main/java/com/phonepe/drove/controller/managed/LeadershipEnsurer.java
@@ -17,8 +17,7 @@ import io.dropwizard.setup.Environment;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.recipes.leader.LeaderLatch;
-import org.apache.curator.framework.recipes.leader.LeaderLatchListener;
+import org.apache.curator.framework.recipes.leader.*;
 import org.eclipse.jetty.server.Server;
 import ru.vyarus.dropwizard.guice.module.installer.order.Order;
 
@@ -27,6 +26,7 @@ import javax.inject.Singleton;
 import java.time.Duration;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -39,15 +39,75 @@ import java.util.concurrent.locks.ReentrantLock;
 public class LeadershipEnsurer implements Managed, ServerLifecycleListener {
     private final NodeDataStore nodeDataStore;
     private final DroveEventBus eventBus;
-    private final LeaderLatch leaderLatch;
+    private final LeaderSelector leaderLatch;
     private final ScheduledSignal checkLeadership = new ScheduledSignal(Duration.ofSeconds(60));
     private final ConsumingSyncSignal<Boolean> leadershipStateChanged = new ConsumingSyncSignal<>();
     private final Lock stateLock = new ReentrantLock();
     private final AtomicBoolean started = new AtomicBoolean();
-    private final AtomicBoolean connected = new AtomicBoolean(true);
     private final AtomicBoolean leader = new AtomicBoolean();
+    private final ControllerLeadershipListener listener;
 
     private ControllerNodeData currentData;
+
+    private final class ControllerLeadershipListener extends LeaderSelectorListenerAdapter {
+
+        private final AtomicBoolean stopped = new AtomicBoolean();
+        private final Lock stopLock = new ReentrantLock();
+        private final Condition stopCondition = stopLock.newCondition();
+
+        @Override
+        public void takeLeadership(CuratorFramework client) throws Exception {
+            log.info("Acquired leadership for this node.");
+            leader.set(true);
+            refreshNodeState();
+            handleLeadershipUpdate(true, false);
+            stopLock.lock();
+            try {
+                while (!stopped.get()) {
+                    stopCondition.await();
+                }
+            }
+            catch (InterruptedException e) {
+                log.warn("Leadership interrupted...");
+                Thread.currentThread().interrupt();
+            }
+            finally {
+                stopLock.unlock();
+            }
+            leader.set(false);
+            refreshNodeState();
+            handleLeadershipUpdate(false, true);
+            log.info("Relinquished leadership for this node");
+        }
+
+        public void stop() {
+            stopLock.lock();
+            try {
+                stopped.set(true);
+                stopCondition.signalAll();
+            }
+            finally {
+                stopLock.unlock();
+            }
+        }
+
+        private void handleLeadershipUpdate(boolean currLeadershipState, boolean oldState) {
+            if(currLeadershipState != oldState) {
+                if(currLeadershipState) {
+                    log.info("This node became leader");
+                }
+                else {
+                    log.info("This node lost leadership");
+                }
+                eventBus.publish(new DroveClusterEvent(
+                        currLeadershipState
+                        ? DroveEventType.LEADERSHIP_ACQUIRED
+                        : DroveEventType.LEADERSHIP_LOST,
+                        EventUtils.controllerMetadata()));
+                leadershipStateChanged.dispatch(currLeadershipState);
+            }
+        }
+    }
 
     @SuppressWarnings("java:S1075")
     @Inject
@@ -58,49 +118,25 @@ public class LeadershipEnsurer implements Managed, ServerLifecycleListener {
             DroveEventBus eventBus) {
         this.nodeDataStore = nodeDataStore;
         this.eventBus = eventBus;
-        val path = "/leadership";
+        this.listener = new ControllerLeadershipListener();
+        val path = "/leaderselection";
 
-        this.leaderLatch = new LeaderLatch(curatorFramework, path);
-        val leaderLatchListener = new LeaderLatchListener() {
-            @Override
-            public void isLeader() {
-                log.info("This node became leader. Updating state.");
-                refreshNodeState();
-            }
-
-            @Override
-            public void notLeader() {
-                log.info("This node lost leadership. Updating state.");
-                refreshNodeState();
-            }
-        };
-        curatorFramework.getConnectionStateListenable().addListener((client, newState) -> {
-            log.info("Zk connection state changed to: {}", newState);
-            switch (newState) {
-                case CONNECTED, RECONNECTED -> {
-                    connected.set(true);
-                    log.info("Node is connected to Zk");
-                }
-                case SUSPENDED, LOST, READ_ONLY -> {
-                    connected.set(false);
-                    log.info("Node is disconnected from Zk");
-                }
-            }
-        });
-        this.leaderLatch.addListener(leaderLatchListener);
+        this.leaderLatch = new LeaderSelector(curatorFramework, path, listener);
+        leaderLatch.autoRequeue();
         this.checkLeadership.connect(this::refresh);
         environment.lifecycle().addServerLifecycleListener(this);
     }
 
     @Override
     public void start() throws Exception {
-        //Nothing to do here. Latch will be started once server starts listening
+        //Nothing to do here. Selector will be started once server starts listening
     }
 
     @Override
     public void stop() throws Exception {
         log.debug("Shutting down {}", this.getClass().getSimpleName());
         if(started.get()) {
+            listener.stop();
             leaderLatch.close();
         }
         started.set(false);
@@ -133,11 +169,9 @@ public class LeadershipEnsurer implements Managed, ServerLifecycleListener {
     }
 
     private void refreshNodeState(int port, NodeTransportType transportType, String hostname) {
-        val currLeadershipState = connected.get() && this.leaderLatch.hasLeadership();
-        val oldState = leader.getAndSet(currLeadershipState);
         stateLock.lock();
         try {
-            currentData = new ControllerNodeData(hostname, port, transportType, new Date(), currLeadershipState);
+            currentData = new ControllerNodeData(hostname, port, transportType, new Date(), isLeader());
             nodeDataStore.updateNodeData(currentData);
             log.info("Node created for this controller. Data: {}", currentData);
             leaderLatch.start();
@@ -150,7 +184,6 @@ public class LeadershipEnsurer implements Managed, ServerLifecycleListener {
         finally {
             stateLock.unlock();
         }
-        handleLeadershipUpdate(currLeadershipState, oldState);
     }
 
     private void refreshNodeState() {
@@ -158,12 +191,9 @@ public class LeadershipEnsurer implements Managed, ServerLifecycleListener {
             log.error("Ignoring ZK controller data update as server has not started completely");
             return;
         }
-        val currLeadershipState = connected.get() && this.leaderLatch.hasLeadership();
-        val oldState = leader.getAndSet(currLeadershipState);
-
         stateLock.lock();
         try {
-            currentData = ControllerNodeData.from(currentData, currLeadershipState);
+            currentData = ControllerNodeData.from(currentData, isLeader());
             nodeDataStore.updateNodeData(currentData);
         }
         catch (Exception e) {
@@ -171,25 +201,6 @@ public class LeadershipEnsurer implements Managed, ServerLifecycleListener {
         }
         finally {
             stateLock.unlock();
-        }
-        log.trace("Curr state: {} Old State: {}", currLeadershipState, oldState);
-        handleLeadershipUpdate(currLeadershipState, oldState);
-    }
-
-    private void handleLeadershipUpdate(boolean currLeadershipState, boolean oldState) {
-        if(currLeadershipState != oldState) {
-            if(currLeadershipState) {
-                log.info("This node became leader");
-            }
-            else {
-                log.info("This node lost leadership");
-            }
-            eventBus.publish(new DroveClusterEvent(
-                    currLeadershipState
-                    ? DroveEventType.LEADERSHIP_ACQUIRED
-                    : DroveEventType.LEADERSHIP_LOST,
-                    EventUtils.controllerMetadata()));
-            leadershipStateChanged.dispatch(currLeadershipState);
         }
     }
 }
