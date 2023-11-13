2fe0f00739208d45cb25bdf65776abc481909c2e
Bobby Dixit <bobby.dixit@phonepe.com>
2023-11-13 22:46:53 +0530

    additional test for default is added

--- drove-executor/src/test/java/com/phonepe/drove/executor/resourcemgmt/ResourceConfigTest.java
+++ drove-executor/src/test/java/com/phonepe/drove/executor/resourcemgmt/ResourceConfigTest.java
@@ -3,6 +3,8 @@ package com.phonepe.drove.executor.resourcemgmt;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResourceConfigTest {
@@ -12,6 +14,16 @@ class ResourceConfigTest {
                 .setDisableNUMAPinning(false)
                 .setBurstUpConfiguration(new BurstUpConfiguration(true, 10, 10))
                 .isBurstAbleEnabledWithDisablePinning());
+    }
+
+    @Test
+    void validateDefaultValuesAreGeneratedForResourceConfig() {
+        var resourceConfig = new ResourceConfig();
+        assertEquals(Collections.emptySet(), resourceConfig.getOsCores());
+        assertEquals(100, resourceConfig.getExposedMemPercentage());
+        assertFalse(resourceConfig.isDisableNUMAPinning());
+        assertEquals(Collections.emptySet(), resourceConfig.getTags());
+        assertFalse(resourceConfig.getBurstUpConfiguration().isBurstUpEnabled());
     }
 
     @Test
