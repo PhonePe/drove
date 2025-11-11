e5405be3c14200c5f3d0083d8c419208248250ee
Santanu Sinha <santanu.sinha@gmail.com>
2025-11-11 09:31:11 +0530

    Added sfatey locs to leadership observer

--- drove-executor/src/main/java/com/phonepe/drove/executor/discovery/RemoteLeadershipObserver.java
+++ drove-executor/src/main/java/com/phonepe/drove/executor/discovery/RemoteLeadershipObserver.java
@@ -34,9 +34,10 @@ import java.time.Duration;
 import java.util.Date;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.StampedLock;
 
 /**
- *
+ * Checks for cluster leader by pinging the specified controllers in the cluster.
  */
 @Singleton
 @AllArgsConstructor(onConstructor_ = {@Inject})
@@ -48,6 +49,7 @@ public class RemoteLeadershipObserver implements LeadershipObserver {
     private final CloseableHttpClient httpClient;
     private final AtomicReference<ControllerNodeData> leader = new AtomicReference<>(null);
     private final ScheduledSignal scheduledSignal = new ScheduledSignal(Duration.ofSeconds(10));
+    private final StampedLock leaderLock = new StampedLock();
 
     @Override
     public Optional<ControllerNodeData> leader() {
@@ -74,32 +76,50 @@ public class RemoteLeadershipObserver implements LeadershipObserver {
             log.debug("No controller config provided. Nothing will be done");
             return;
         }
-        final var controllers = controllerConfig.getEndpoints();
-        final var leaderController = controllers.stream()
-                .filter(this::pingEndpoint)
-                .findAny()
-                .map(leaderEndpoint -> {
-                    val protocol = leaderEndpoint.getProtocol();
-                    return new ControllerNodeData(leaderEndpoint.getHost(),
-                                                  leaderEndpoint.getPort(),
-                                                  protocol.equals("https") ? NodeTransportType.HTTPS
-                                                                           : NodeTransportType.HTTP,
-                                                  new Date(),
-                                                  true);
-                })
-                .orElse(null);
-        //TODO::LOCK
-        val oldLeader = leader.get();
-        if (null == leaderController) {
-            log.warn("No leader in the cluster");
+        var stamp = leaderLock.tryReadLock();
+        if (stamp == 0) {
+            log.info("Looks like another attempt is already underway. Skipping this leader check");
+            return;
         }
-        val leaderChanged = (null == oldLeader && null != leaderController)
-                || (null != oldLeader && null == leaderController)
-                || (null != oldLeader && (!oldLeader.getHostname()
-                .equals(leaderController.getHostname()) || oldLeader.getPort() != leaderController.getPort()));
-        if (leaderChanged) {
-            leader.set(leaderController);
-            log.info("Leader changed from {} to {}", oldLeader, leaderController);
+        try {
+            final var controllers = controllerConfig.getEndpoints();
+            final var leaderController = controllers.stream()
+                    .filter(this::pingEndpoint)
+                    .findAny()
+                    .map(leaderEndpoint -> {
+                        val protocol = leaderEndpoint.getProtocol();
+                        return new ControllerNodeData(leaderEndpoint.getHost(),
+                                                      leaderEndpoint.getPort(),
+                                                      protocol.equals("https") ? NodeTransportType.HTTPS
+                                                                               : NodeTransportType.HTTP,
+                                                      new Date(),
+                                                      true);
+                    })
+                    .orElse(null);
+
+            val oldLeader = leader.get();
+            if (null == leaderController) {
+                log.warn("No leader in the cluster");
+            }
+            val leaderChanged = (null == oldLeader && null != leaderController)
+                    || (null != oldLeader && null == leaderController)
+                    || (null != oldLeader && (!oldLeader.getHostname()
+                    .equals(leaderController.getHostname()) || oldLeader.getPort() != leaderController.getPort()));
+            if (leaderChanged) {
+                val status = leaderLock.tryConvertToWriteLock(stamp);
+                if (0 == status) {
+                    leaderLock.unlockRead(stamp);
+                    stamp = leaderLock.readLock();
+                }
+                else {
+                    stamp = status;
+                }
+                leader.set(leaderController);
+                log.info("Leader changed from {} to {}", oldLeader, leaderController);
+            }
+        }
+        finally {
+            leaderLock.unlock(stamp);
         }
     }
 
